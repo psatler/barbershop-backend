b34ff2fed841b4fd45f9c52ce95b67c98c1c9f4a
Pablo Satler <pablosatler@gmail.com>
2020-04-13 18:54:53 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+
+  return res.json({
+    message: "Hello, World!"
+  })
+})
+
+app.listen(3333, () => {
+  console.log('Server is listening')
+})
