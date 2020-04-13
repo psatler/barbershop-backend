b7fe3aca969dbc38b79dc9f984ebaccb6c06fc3a
Pablo Satler <pablosatler@gmail.com>
2020-04-13 19:26:43 -0300

    feat: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-
-  return res.json({
-    message: "Hello, World!"
-  })
-})
+app.get('/', (req, res) => res.json({
+  message: 'Hello, World!',
+}));
 
 app.listen(3333, () => {
-  console.log('Server is listening')
-})
+  console.log('Server is listening');
+});
