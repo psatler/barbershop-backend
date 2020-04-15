7b53fa43972d78e2dda80f18eb2ee10b0624e3ad
Pablo Satler <pablosatler@gmail.com>
2020-04-15 20:42:11 -0300

    chore: add decorators support and use them in the Appointments model
    - use typeORM decorators to map the model to the respective table in the database

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,21 +1,25 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 /**
  * Appointment entity
  */
+@Entity('appointments') // refering to the appointments table in the database
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
 
   // using the Omit to indicate all attributes but id will come from the constructor
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  //   this.id = uuid();
+  //   this.provider = provider;
+  //   this.date = date;
+  // }
 }
 
 export default Appointment;
