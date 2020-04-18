0e0e23e274d8b8136953913089d034621e0b68e1
Pablo Satler <pablosatler@gmail.com>
2020-04-17 23:57:37 -0300

    chore: update the user avatar and save it to the database
    - save the avatar path to the database
    - perform validations on the service
    - delete old avatar if user already have one before updating the avatar

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface RequestDTO {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: RequestDTO): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar!');
+    }
+
+    if (user.avatar) {
+      // check if user already have an avatar saved. If so, we deleted it first
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath); // we delete the file
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user); // saving the user with the updated avatar
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
