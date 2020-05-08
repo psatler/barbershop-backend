bc47926609b404b308fb360ab4a6f1fc4301866a
Pablo Satler <pablosatler@gmail.com>
2020-05-08 20:28:38 -0300

    test: create forgot email password service in a TDD way
    - create first test for sending a recovery email and make it pass

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+// import User from '../infra/typeorm/entities/User';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequestDTO {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  // dependency inversion
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+  ) {}
+
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import IStorageProvider from './storageProvider/models/IStorageProvider';
 import DiskStorageProvider from './storageProvider/implementations/DiskStorageProvider';
 
+// import IMailProvider from './MailProvider/models/IMailProvider'
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
