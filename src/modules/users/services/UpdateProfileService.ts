86ebc37f7196718ebd918001d75ba53592abaa09
Pablo Satler <pablosatler@gmail.com>
2020-05-10 20:49:22 -0300

    chore: create update profile service and its tests

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,73 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/hashProvider/models/IHashProvider';
+
+interface IRequestDTO {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfile {
+  // dependency inversion
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    user_id,
+    name,
+    email,
+    password,
+    old_password,
+  }: IRequestDTO): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const userWithUpdatedEmail = await this.usersRepository.findByEmail(email);
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
+      throw new AppError('E-mail already in use');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        'You need to inform the old passoword to set a new one',
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password, // this is the hash stored in the database
+      );
+
+      if (!checkOldPassword) {
+        throw new AppError('Old password does not match');
+      }
+    }
+
+    if (password) {
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    return this.usersRepository.save(user);
+  }
+}
+
+export default UpdateProfile;
