155e63c8516aa449afd48ed18f9badc176a62e30
Pablo Satler <pablosatler@gmail.com>
2020-05-14 20:52:52 -0300

    feat: add a rate limiter
    - add a simple rate limiter to the application with node-rate-limiter-flexible

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-import { parseISO } from 'date-fns';
+// import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
