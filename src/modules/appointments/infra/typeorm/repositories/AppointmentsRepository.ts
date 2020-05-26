ca6b965706c725add30f34595de0fca40aa048d8
Pablo Satler <pablosatler@gmail.com>
2020-05-26 16:46:10 -0300

    chore: list provider appointments in Ascending order by date
    - add order ASC for date when querying the database for appointments at a given day

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -68,6 +68,9 @@ class AppointmentsRepository implements IAppointmentsRepository {
         ),
       },
       relations: ['user'],
+      order: {
+        date: 'ASC',
+      },
     });
 
     return appointments;
